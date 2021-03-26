986d1fe14dbe7a479bb68c5a2b2e0125fbc8e5b6
JuanGuzmanH <juand.guzmanh@utadeo.edu.co>
2021-03-26 16:55:34 -0500

    Lista episodios

--- src/components/Episodios/EpisodioCard.js
+++ src/components/Episodios/EpisodioCard.js
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from 'react'
 
 export const EpisodioCard = ({ Episodio }) => {
     const [episodio1, setEpisodio1] = useState({ id: '', titulo: '', caracteres: ''});
+    const{characters}=Episodio;
     const getEpisodios = async () => {
         const { id, title, characters} = Episodio;
         
@@ -18,7 +19,9 @@ export const EpisodioCard = ({ Episodio }) => {
                 
                 <div className="card-content" style={{background: 'linear-gradient(0deg , rgb(4, 4, 78), rgb(38, 65, 92) 10%, rgb(4, 4, 78))'}}>
                     <p style={{color: 'white'}}>Titulo: {episodio1.titulo}</p>
-                    <p style={{color: 'white'}}>Personajes: {episodio1.caracteres} </p>
+                    <p style={{color: 'white'}}><ul>Personajes: {characters.map(
+                        caract=>( <li key={caract} id="caracteres">{caract}</li>)
+                    )} </ul></p>
                 </div>
             </div>
         </div>
